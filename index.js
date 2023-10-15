29959b2a88a324bc525d85b92529b744dc2a3d56
Azmal <azmalgazi8@gmail.com>
2023-10-15 12:54:05 +0600

    secure Api to implement JWT

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken');
+const  morgan = require('morgan')
 const port = process.env.PORT || 5000
 
 
@@ -15,6 +16,7 @@ const corsOptions = {
   }
 app.use(cors());
 app.use(express.json());
+app.use (morgan('dev'))
 
 
 
@@ -33,10 +35,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+//Validate jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error:true, message:'Unauthorized Access'})
+  }
+  // console.log(authorization)
+  const token= authorization.split(' ')[1]
+ 
+  console.log(token)
+  //token verify
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error:true, message:'Unauthorized Access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+
+}
+
 async function run() {
   try {
-
-
     const usersCollection = client.db('aircnc').collection('users')
     const roomsCollection = client.db('aircnc').collection('rooms')
     const bookingsCollection = client.db('aircnc').collection('bookings')
@@ -44,6 +65,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+
+    // Generate Jwt token
+        app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h',
+    })
+    
+      res.send({token});
+    })
+
 
    // Save users
     app.put('/users/:email', async (req, res) => {
@@ -86,8 +118,13 @@ async function run() {
     })
 
     //get filtered rooms for host
-    app.get('/rooms/:email', async (req, res) => {
+    app.get('/rooms/:email',verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email
+      console.log(decodedEmail)
       const email= req.params.email;
+      if(email !==decodedEmail){
+          return res.status(403).send({error:true, message:'Forbidden Access'})
+      }
       const query = { 'host.email' : email };
       const result = await roomsCollection.find(query).toArray();
       res.send(result);
@@ -137,6 +174,19 @@ app.get('/bookings', async (req, res) => {
   const result = await bookingsCollection.find(query).toArray();
   res.send(result)
 })
+
+
+//get bookings for host
+app.get('/bookings/host', async (req, res) => {
+  const email = req.query.email;
+  if(!email) {
+    res.send([])
+  }
+  const query ={host: email}
+  const result = await bookingsCollection.find(query).toArray();
+  res.send(result)
+})
+
 
 //saved a booking in database
 app.post('/bookings', async (req, res) => {
